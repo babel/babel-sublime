1172e2ae52e19ab977c3242a29fbd64711490924
Andres Suarez <zertosh@gmail.com>
2015-02-13 17:00:03 -0500

    Fix overly aggressive jsx tag matching
    Now it'll only match jsx tags if the identifier is immediately followed by a space or (self-)closing bracket. This is to avoid the case where unspaced greater-than comparisons are confused with jsx tags. Also narrowed the allowed chars for identifiers to match the spec.

---
+++ test/jsx-tag-name.jsx
@@ -0,0 +1,29 @@
+var a = b<cat()
+var a = b < cat()
+var a = b <cat[1]
+var a = b < cat[1]
+var a = b <cat?
+var a = b < cat?
+var a = b <cat===
+var a = b < cat===
+var a = b <cat&
+var a = b < cat&&
+var a = b <cat++
+var a = b < cat++
+var a = b <cat--
+var a = b < cat--
+
+var a = b <cat /*comment*/ />
+var a = b <cat />
+var a = b <cat/>
+var a = b <cat-dog />
+var a = b <cat-dog/>
+var a = b <cat_dog />
+var a = b <cat-dog />
+var a = b <cat.dog />
+
+var a = b <cat.dog/*comment*/ />
+//
+var a = b <cat- />
+//
+var a = b <cat. />
