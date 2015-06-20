f6aac282777fa1c4df59bd737cd32586ad3d6fa6
Andres Suarez <zertosh@gmail.com>
2015-06-20 18:03:35 -0400

    more test files

---
+++ test/js-functions.js
@@ -0,0 +1,100 @@
+let Sound = {
+  play() {},
+  play(arg) {},
+  static play() {},
+  static play(arg) {},
+  *play() {},
+  *play(arg) {},
+  static *play() {},
+  static *play(arg) {},
+  async play() {},
+  static async play() {},
+  static async play(arg) {},
+  get play() {},
+  static get play() {},
+  get play(arg) {},
+  static get play(arg) {},
+  set play() {},
+  static set play() {},
+  set play(arg) {},
+  static set play(arg) {},
+  play: function() {},
+  play: function(arg) {},
+  play: function*() {},
+  play: function*(arg) {},
+  play: async function() {},
+  play: async function*() {},
+  play: async (arg) => {},
+  play: async arg => {},
+  play: (arg) => {},
+  play: arg => {},
+  play: () => {},
+  "play": function() {},
+  "play": function(arg) {},
+  "play": function*() {},
+  "play": function*(arg) {},
+  "play": async function() {},
+  "play": async function*() {},
+  "play": async (arg) => {},
+  "play": async arg => {},
+  "play": (arg) => {},
+  "play": arg => {},
+  "play": () => {},
+}
+
+Sound.prototype
+Sound.prototype = {}
+
+Sound.prototype.play = function() {}
+Sound.prototype.play = function(arg) {}
+Sound.prototype.play = function*() {}
+Sound.prototype.play = function*(arg) {}
+Sound.prototype.play = async function() {}
+Sound.prototype.play = async function*() {}
+Sound.prototype.play = async (arg) => {}
+Sound.prototype.play = async arg => {}
+Sound.prototype.play = (arg) => {}
+Sound.prototype.play = arg => {}
+Sound.prototype.play = () => {}
+
+Sound.play = function() {}
+Sound.play = function(arg) {}
+Sound.play = function*() {}
+Sound.play = function*(arg) {}
+Sound.play = async function() {}
+Sound.play = async function*() {}
+Sound.play = async (arg) => {}
+Sound.play = async arg => {}
+Sound.play = (arg) => {}
+Sound.play = arg => {}
+Sound.play = () => {}
+
+play = function() {}
+play = function(arg) {}
+play = function*() {}
+play = function*(arg) {}
+play = async function() {}
+play = async function*() {}
+play = async (arg) => {}
+play = async arg => {}
+play = (arg) => {}
+play = arg => {}
+play = () => {}
+
+return function() {}
+return function(arg) {}
+return function*() {}
+return function*(arg) {}
+return async function() {}
+return async function*() {}
+return async (arg) => {}
+return async arg => {}
+return (arg) => {}
+return arg => {}
+return () => {}
+
+/////////
+
+yieldgenfunc;
+yield genfunc;
+yield* genfunc;
