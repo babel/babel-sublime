6249f715632598660c945d4c17c125280b983de5
Andres Suarez <zertosh@gmail.com>
2015-07-12 20:27:30 -0400

    lots of fixes for "class"

--- samples/jsx-full-react-class.jsx
+++ samples/jsx-full-react-class.jsx
@@ -30,3 +30,27 @@ export default React.createClass({
     );
   }
 });
+
+// With ES7+ Property Initializers
+
+export class Counter extends React.Component {
+  static propTypes = { initialCount: React.PropTypes.number };
+  static defaultProps = { initialCount: 0 };
+  static childContextTypes = () => {
+    return {
+      app: React.PropTypes.instanceOf(App).isRequired
+    }
+  };
+  getChildContext = makeGetChildContext();
+  state = { count: this.props.initialCount };
+  tick() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  render() {
+    return (
+      <div onClick={this.tick.bind(this)}>
+        Clicks: {this.state.count}
+      </div>
+    );
+  }
+}
