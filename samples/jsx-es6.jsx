9600651f83a50bc84d407b5f9215b0455fa342f7
Andres Suarez <zertosh@gmail.com>
2015-09-15 21:08:25 -0700

    fix jsx with multiline strings

--- samples/jsx-es6.jsx
+++ samples/jsx-es6.jsx
@@ -23,6 +23,8 @@ export default React.createClass({
   render() {
     var {email} = this.state;
     var list = this.props.secondary.map(pic => <img src={pic} />)}
+    var multilineAttr = <a desc="ab
+      cdef"></a>
     return (
       <div {...this.props} overlay={<div>test</div>}>
         <ns:tag></ns:tag>
