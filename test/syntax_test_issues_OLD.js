258fada515a496fd79d5555103df1958e0ffe08b
Andres Suarez <zertosh@gmail.com>
2016-11-27 20:23:40 -0500

    Add tests for #244

--- test/syntax_test_issues_OLD.js
+++ test/syntax_test_issues_OLD.js
@@ -1,6 +1,8 @@
 // SYNTAX TEST "Packages/babel-sublime/JavaScript (Babel).tmLanguage"
 
-/* https://github.com/babel/babel-sublime/issues/243 */
+/**
+ * https://github.com/babel/babel-sublime/issues/243
+ */
 
 class Foo extends create('Bar') {}
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.class
@@ -15,3 +17,63 @@ class Foo extends create('Bar') {}
 //                            ^ meta.class meta.class.extends meta.method punctuation.definition.parameters.end
 //                              ^ meta.class meta.brace.curly.begin
 //                               ^ meta.class meta.brace.curly.end
+
+
+/**
+ * https://github.com/babel/babel-sublime/issues/244
+ */
+
+var foo = bar ? "+" + baz : qux;
+//^ storage.type
+//<- source.js
+//  ^^^ variable.other.readwrite
+//      ^ keyword.operator.assignment
+//        ^^^ variable.other.readwrite
+//            ^ keyword.operator.ternary
+//              ^^^ string.quoted
+//              ^ string.quoted punctuation.definition.string.begin
+//                ^ string.quoted punctuation.definition.string.end
+//                  ^ keyword.operator.arithmetic
+//                    ^^^^^ constant.other.object.key
+//                    ^^^ constant.other.object.key string.unquoted.label
+//                        ^ constant.other.object.key punctuation.separator.key-value
+//                          ^^^ variable.other.readwrite
+//                             ^ punctuation.terminator.statement
+
+var foo = bar ? "+" + baz + baz2 : qux;
+//^ storage.type
+//<- source.js
+//  ^^^ variable.other.readwrite
+//      ^ keyword.operator.assignment
+//        ^^^ variable.other.readwrite
+//            ^ keyword.operator.ternary
+//              ^^^ string.quoted
+//              ^ string.quoted punctuation.definition.string.begin
+//                ^ string.quoted punctuation.definition.string.end
+//                  ^ keyword.operator.arithmetic
+//                    ^^^ variable.other.readwrite
+//                        ^ keyword.operator.arithmetic
+//                          ^^^^^^ constant.other.object.key
+//                          ^^^^ constant.other.object.key string.unquoted.label
+//                               ^ constant.other.object.key punctuation.separator.key-value
+//                                 ^^^ variable.other.readwrite
+//                                    ^ punctuation.terminator.statement
+
+var foo = bar ? "+" + (baz) : qux;
+//^ storage.type
+//<- source.js
+//  ^^^ variable.other.readwrite
+//      ^ keyword.operator.assignment
+//        ^^^ variable.other.readwrite
+//            ^ keyword.operator.ternary
+//              ^^^ string.quoted
+//              ^ string.quoted punctuation.definition.string.begin
+//                ^ string.quoted punctuation.definition.string.end
+//                  ^ keyword.operator.arithmetic
+//                    ^ meta.group.braces.round meta.brace.round.begin
+//                     ^^^ meta.group.braces.round variable.other.readwrite
+//                        ^ meta.group.braces.round meta.brace.round.end
+//                          ^ punctuation.separator.key-value
+//                            ^^^ variable.other.readwrite
+//                               ^ punctuation.terminator.statement
+
