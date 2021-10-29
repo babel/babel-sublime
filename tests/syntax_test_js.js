02edb9da250a2ea902572a30ca4f114a4d5cbd24
Thom Smith <thom@thomthesmith.com>
2021-10-29 10:25:02 -0400

    Integrate upstream changes.

--- tests/syntax_test_js.js
+++ tests/syntax_test_js.js
@@ -73,10 +73,6 @@
 //   ^^^^ comment.block.documentation.js
 //       ^ - comment
 
-/// <foo bar="baz"/>
-// <- comment.line.triple-slash.js punctuation.definition.comment.js
-//^^^^^^^^^^^^^^^^^^^ comment.line.triple-slash.js - meta.preprocessor
-
 //// <foo bar="baz"/>
 // <- comment.line.other.js punctuation.definition.comment.js
 //^^^^^^^^^^^^^^^^^^^^ comment.line.other.js - meta.preprocessor
---
+++ tests/syntax_test_js_not_typescript.js
@@ -0,0 +1,5 @@
+// SYNTAX TEST "Packages/Babel/JavaScript (Babel).sublime-syntax"
+
+/// <foo bar="baz"/>
+// <- comment.line.triple-slash.js punctuation.definition.comment.js
+//^^^^^^^^^^^^^^^^^^^ comment.line.triple-slash.js - meta.preprocessor
